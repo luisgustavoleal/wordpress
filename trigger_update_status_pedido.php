5dc57d3f4a41f5d26cdb7b4718e36b8663b633e7
Luis Gustavo Marucco Lins Leal <contato@luisgustavo.com.br>
2021-06-25 19:05:40 +0100

    Create trigger_update_status_pedido.php

---
+++ trigger_update_status_pedido.php
@@ -0,0 +1,13 @@
+/* 
+
+ */
+
+add_action( 'woocommerce_thankyou', 'woocommerce_thankyou_change_order_status', 10, 1 );
+function woocommerce_thankyou_change_order_status( $order_id ){
+    if( ! $order_id ) return;
+
+    $order = wc_get_order( $order_id );
+
+    if( $order->get_status() == 'processing'  || $order->get_status() == 'on-hold' )
+        $order->update_status( 'pending' );
+}
